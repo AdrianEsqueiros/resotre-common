b17e6318a5806dbad73b3b2e91812c95f9e2fcac
Adrian <u201718169@upc.edu.pe>
2022-06-04 04:25:00 -0500

    Initial commit

---
+++ src/main/java/com/example/resotrecommon/ResotreCommonApplication.java
@@ -0,0 +1,13 @@
+package com.example.resotrecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResotreCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResotreCommonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resotrecommon/ResotreCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resotrecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResotreCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
